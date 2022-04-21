84d97655fe98e944df9d84ef995caf0da01b09cb
sarahrubia <sarahrubia22@gmail.com>
2022-04-21 19:15:31 -0300

    fix: mascara do formulario e soma de valores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,52 @@
-
 //  Persistir dados no LocalStorage
 
-var mercadoriasRaw = localStorage.getItem('listaDeMercadorias');
+var mercadoriasRaw = localStorage.getItem("listaDeMercadorias");
 
 if (mercadoriasRaw != null) {
-    var listaDeMercadorias = JSON.parse(mercadoriasRaw)
+  var listaDeMercadorias = JSON.parse(mercadoriasRaw);
 } else {
-    var listaDeMercadorias = [];
-};
-
+  var listaDeMercadorias = [];
+}
 
 //  Validação e máscara do formulário
 
 function mascaraValor(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    if ((/[0-9]+/g).test(e.key) && e.target.value.length < 14) {
-        e.target.value += e.key
-    }
+  if (/[0-9]+/g.test(e.key) && e.target.value.length < 14) {
+    e.target.value += e.key;
+  }
 
-    var meuInput = Number(e.target.value.replace(/[^0-9]+/g, ''));
-    meuInput = (meuInput/100)
-    inputFormatado = meuInput.toLocaleString('pt-BR', {
-        style: 'currency',
-        currency: 'BRL',
-        minimumFractionDigits: 2,
-    });
+  var meuInput = Number(e.target.value.replace(/[^0-9]+/g, ""));
+  meuInput = meuInput / 100;
+  inputFormatado = meuInput.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+    minimumFractionDigits: 2,
+  });
 
-    console.log(inputFormatado)
-    e.target.value = inputFormatado
+  console.log(inputFormatado);
+  e.target.value = inputFormatado;
 }
-
 
 // Listagem das transações
 
-
 function adicionaTabela() {
-    
-    linhasExistentes = [...document.querySelectorAll('table.lista-mercadoria tbody .conteudo-dinamico')];
+  linhasExistentes = [
+    ...document.querySelectorAll(
+      "table.lista-mercadoria tbody .conteudo-dinamico"
+    ),
+  ];
 
-    linhasExistentes.forEach(element => {
-        element.remove();
-        
-    });
+  linhasExistentes.forEach((element) => {
+    element.remove();
+  });
 
-    for (merc in listaDeMercadorias) {
-        document.querySelector('table.lista-mercadoria tbody').innerHTML += `
+  for (merc in listaDeMercadorias) {
+    document.querySelector("table.lista-mercadoria tbody").innerHTML += `
         <tr class='conteudo-dinamico'>
             <td>
-            ${listaDeMercadorias[merc]["tipo-transacao"] === '-' ? '-' : '+'}
+            ${listaDeMercadorias[merc]["tipo-transacao"] === "-" ? "-" : "+"}
             </td>
             <td>
             ${listaDeMercadorias[merc]["nome-mercadoria"]}
@@ -59,26 +57,33 @@ function adicionaTabela() {
             <tr class="linha-tabela">
                 <td class="linha-tabela" colspan="3"></td>
             </tr>
-        </tr>`
+        </tr>`;
+  }
+
+  // Cálculo do valor total
+
+  var total = 0;
+
+  for (valor in listaDeMercadorias) {
+    valores = parseFloat(
+      listaDeMercadorias[valor]["valor-mercadoria"]
+        .replace(/[^0-9]/g, "")
+        .replace(",", ".")
+    );
+
+    if (listaDeMercadorias[valor]["tipo-transacao"] === "-") {
+      total -= valores / 100;
+    } else if (listaDeMercadorias[valor]["tipo-transacao"] === "+") {
+      total += valores / 100;
     }
-    
-    // Cálculo do valor total
+  }
 
-    var total = 0;
+  var totalFormatado = total.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
 
-    for (valor in listaDeMercadorias) {
-        valores = parseFloat(listaDeMercadorias[valor]["valor-mercadoria"].replace(/[^0-9]/g,"").replace(",", "."))
-
-        if (listaDeMercadorias[valor]["tipo-transacao"] === '-') {
-            total -= valores / 100
-        } else if (listaDeMercadorias[valor]["tipo-transacao"] === '+') {
-            total += valores / 100
-        }
-    }
-
-    var totalFormatado = total.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
-
-    document.querySelector('table.lista-mercadoria tfoot').innerHTML += `
+  document.querySelector("table.lista-mercadoria tfoot").innerHTML += `
     <tr>
         <td class="linha-tabela" style="padding-top: 4px; border-top: 2px solid #979797; border-bottom: 2px solid #979797" colspan="3"></td>
     </tr>
@@ -86,52 +91,58 @@ function adicionaTabela() {
         <td></td>
         <td><strong>Total</strong></td>
         <td style="text-align:right; display: block;" id="valor-total"><strong>${totalFormatado}</strong>
-        <p>${(total > 0) ? "[LUCRO]" : ((total < 0) ? "[PREJUÍZO]" : '')}</p>
+        <p>${total > 0 ? "[LUCRO]" : total < 0 ? "[PREJUÍZO]" : ""}</p>
         </td>
-    </tr>`
+    </tr>`;
 
-    // Adicionando tabela para quando não houver transações cadastradas
+  // Adicionando tabela para quando não houver transações cadastradas
 
-    if (mercadoriasRaw == null) {
-        document.querySelector('table.lista-mercadoria tbody').innerHTML += `
+  if (mercadoriasRaw == null) {
+    document.querySelector("table.lista-mercadoria tbody").innerHTML += `
         <tr class='conteudo-dinamico'>
             <td colspan="3" style ="text-align: center; padding-bottom: 10px"}>
                 Nenhuma transação cadastrada.
             </td>
-        <tr>`
-    }
-};
+        <tr>`;
+  }
+}
 
 adicionaTabela();
 
-// Excluindo listagem 
+// Excluindo listagem
 
 function limparDados(merc) {
-    confirm('Tem certeza que deseja limpar os dados da pagina?');
-    localStorage.clear();
-    document.getElementById('inicio').click();
-};
+  confirm("Tem certeza que deseja limpar os dados da pagina?");
+  localStorage.clear();
+  document.getElementById("inicio").click();
+}
 
 // Cadastrando transações
 
 function cadastroTransacao(evt) {
-    evt.preventDefault();
-    console.log(evt)
-    
-    var mercadoriasRaw = localStorage.getItem('listaDeMercadorias');
+  evt.preventDefault();
 
-    if (mercadoriasRaw != null) {
-        var listaDeMercadorias = JSON.parse(mercadoriasRaw)
-    } else {
-        var listaDeMercadorias = [];
-};
+  var mercadoriasRaw = localStorage.getItem("listaDeMercadorias");
 
-    listaDeMercadorias.push({
-        "tipo-transacao": evt.target.elements["tipo-transacao"].value,
-        "nome-mercadoria": evt.target.elements["nome-mercadoria"].value,
-        "valor-mercadoria": evt.target.elements["valor-mercadoria"].value, 
-    });
+  if (mercadoriasRaw != null) {
+    var listaDeMercadorias = JSON.parse(mercadoriasRaw);
+  } else {
+    var listaDeMercadorias = [];
+  }
 
-    localStorage.setItem('listaDeMercadorias', JSON.stringify(listaDeMercadorias));
-    document.getElementById('inicio').click();
+  listaDeMercadorias.push({
+    "tipo-transacao": evt.target.elements["tipo-transacao"].value,
+    "nome-mercadoria": evt.target.elements["nome-mercadoria"].value,
+    "valor-mercadoria": evt.target.elements["valor-mercadoria"].value,
+  });
+
+  localStorage.setItem(
+    "listaDeMercadorias",
+    JSON.stringify(listaDeMercadorias)
+  );
+
+  document.getElementById("inicio").click();
+
 }
+
+// Falta atualizar a tabela de transações e limpar os dados sem atualizar a página
